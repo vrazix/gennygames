50936a67b43f2f3c5ade7fc9ffd68da3f1fde1a9
vrazix <vrazix@gmail.com>
2018-06-28 20:17:44 -0400

    initial commit

---
+++ frontend.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return """<h1>Hello world!</h1>"""
+
+if __name__ == '__main__':
+    app.run(use_reloader=True)
