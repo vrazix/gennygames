74a423441474cefc881c053099735d265638e6a7
vrazix <vrazix@gmail.com>
2018-07-02 08:45:17 -0400

    tabs to spaces

--- boardgames.py
+++ boardgames.py
@@ -8,77 +8,77 @@ import random
 
 
 def least_recent_play(game):
-	'''Return a value for a game based on it's overall least recent play.'''
-	if game['plays']:
-		return dateparser.parse(sorted(game['plays'], key=lambda play: dateparser.parse(play['playDate']))[0]['playDate'])
-	else:
-		return dateparser.parse('jan 1 1980')
+    '''Return a value for a game based on it's overall least recent play.'''
+    if game['plays']:
+        return dateparser.parse(sorted(game['plays'], key=lambda play: dateparser.parse(play['playDate']))[0]['playDate'])
+    else:
+        return dateparser.parse('jan 1 1980')
 
 
 def viable_game_list(games, n_players, an_owner, sort_option='least_recent'):
-	'''Return a list of games that you can play!
+    '''Return a list of games that you can play!
 
-	`games` is the bg_data['games'] from 'master_bg_data.json'
-	`n_players` is how many players want to play
-	`an_owner` is a single name that indicates where you are.
+    `games` is the bg_data['games'] from 'master_bg_data.json'
+    `n_players` is how many players want to play
+    `an_owner` is a single name that indicates where you are.
 
-	As of 6/25/2018 that means:
-	Tricia = SamR = Ryan = Travis
-	SamD = Andrew = pixels
+    As of 6/25/2018 that means:
+    Tricia = SamR = Ryan = Travis
+    SamD = Andrew = pixels
 
-	with all other names being unique.'''
+    with all other names being unique.'''
 
-	owner_decode = {'samd'		: lambda game: 'andrew' in game['Owner'].lower(),
-					'andrew'	: lambda game: 'andrew' in game['Owner'].lower(),
-					'pixels'	: lambda game: 'andrew' in game['Owner'].lower(),
-					'tricia'	: lambda game: 'ryan' in game['Owner'].lower() or 'tricia' in game['Owner'].lower(),
-					'samr'		: lambda game: 'ryan' in game['Owner'].lower() or 'tricia' in game['Owner'].lower(),
-					'travis'	: lambda game: 'ryan' in game['Owner'].lower() or 'tricia' in game['Owner'].lower(),
-					'ryan'		: lambda game: 'ryan' in game['Owner'].lower() or 'tricia' in game['Owner'].lower(),
-					'alex'		: lambda game: 'alex' in game['Owner'].lower(),
-					'tim'		: lambda game: 'tim' in game['Owner'].lower(),
-					'evan'		: lambda game: 'evan' in game['Owner'].lower(),
-					}
+    owner_decode = {'samd'      : lambda game: 'andrew' in game['Owner'].lower(),
+                    'andrew'    : lambda game: 'andrew' in game['Owner'].lower(),
+                    'pixels'    : lambda game: 'andrew' in game['Owner'].lower(),
+                    'tricia'    : lambda game: 'ryan' in game['Owner'].lower() or 'tricia' in game['Owner'].lower(),
+                    'samr'      : lambda game: 'ryan' in game['Owner'].lower() or 'tricia' in game['Owner'].lower(),
+                    'travis'    : lambda game: 'ryan' in game['Owner'].lower() or 'tricia' in game['Owner'].lower(),
+                    'ryan'      : lambda game: 'ryan' in game['Owner'].lower() or 'tricia' in game['Owner'].lower(),
+                    'alex'      : lambda game: 'alex' in game['Owner'].lower(),
+                    'tim'       : lambda game: 'tim' in game['Owner'].lower(),
+                    'evan'      : lambda game: 'evan' in game['Owner'].lower(),
+                    }
 
-	viable_games = []
-	available_games = {bgg_id: game for bgg_id, game in games.items() if owner_decode[an_owner](game)}
-	for bgg_id, game in available_games.items():
-		if int(game['Max Players']) >= n_players >= int(game['Min Players']):
-			viable_games.append(game)
+    viable_games = []
+    available_games = {bgg_id: game for bgg_id, game in games.items() if owner_decode[an_owner](game)}
+    for bgg_id, game in available_games.items():
+        if int(game['Max Players']) >= n_players >= int(game['Min Players']):
+            viable_games.append(game)
 
-	sort_func = {'least_recent': least_recent_play,
-				 'random'	   : random.random}
+    sort_func = {'least_recent': least_recent_play,
+                 'random'      : random.random}
 
-	viable_games.sort(key=sort_func[sort_option])
+    viable_games.sort(key=sort_func[sort_option])
 
-	return viable_games
+    return viable_games
 
 
 def show_me_games(viable_game_list):
-	for game in viable_game_list:
-		if game['plays']:
-			print(game['Name'], least_recent_play(game))
-		else:
-			print(game['Name'], 'No recorded play.')
+    for game in viable_game_list:
+        if game['plays']:
+            print(game['Name'], least_recent_play(game))
+        else:
+            print(game['Name'], 'No recorded play.')
 
 
 def load_data(json_file='master_bg_data.json'):
-	'''Return games from the json file.'''
+    '''Return games from the json file.'''
 
-	with open(json_file, 'r') as inf:
-		bg_data = json.load(inf)
+    with open(json_file, 'r') as inf:
+        bg_data = json.load(inf)
 
-	games = bg_data['games']
-	return games
+    games = bg_data['games']
+    return games
 
 
 if __name__ == '__main__':
-	#with open('master_bg_data.json', 'r') as inf:
-	#	bg_data = json.load(inf)
+    #with open('master_bg_data.json', 'r') as inf:
+    #   bg_data = json.load(inf)
 
-	#games = bg_data['games']
-	
-	#players = refactor_json_data(bg_data['players'], 'id')
-	#locations = refactor_json_data(bg_data['locations'], 'id')
-	games = load_data()
-	show_me_games(viable_game_list(games, 3, 'tricia'))
+    #games = bg_data['games']
+    
+    #players = refactor_json_data(bg_data['players'], 'id')
+    #locations = refactor_json_data(bg_data['locations'], 'id')
+    games = load_data()
+    show_me_games(viable_game_list(games, 3, 'tricia'))
--- utils.py
+++ utils.py
@@ -2,35 +2,35 @@
 
 
 def read_csv(fn, delimiter='\t'):
-	'''Return a list of dictionaries with the contents of the file.
+    '''Return a list of dictionaries with the contents of the file.
 
-	Assumes newline characters at the end of each line, which are trimmed.'''
+    Assumes newline characters at the end of each line, which are trimmed.'''
 
-	objects = []
-	with open(fn, 'r') as inf:
-		iterator = inf.readlines()
-		header = iterator[0]
-		fields = header[:-1].split(delimiter)
+    objects = []
+    with open(fn, 'r') as inf:
+        iterator = inf.readlines()
+        header = iterator[0]
+        fields = header[:-1].split(delimiter)
 
-		for line in iterator[1:]:
-			objects.append({k: v for k, v in zip(fields, line[:-1].split(delimiter))})
-	
-	return objects
+        for line in iterator[1:]:
+            objects.append({k: v for k, v in zip(fields, line[:-1].split(delimiter))})
+    
+    return objects
 
 
 def refactor_json_data(json_list, master_key):
-	'''Refactor a json list into a dictionary using
-	the key `key` from each dictionary in the list.
+    '''Refactor a json list into a dictionary using
+    the key `key` from each dictionary in the list.
 
-	>>> refactor_json_data([{'id': 1, 'data': 'stuff'},
-							{'id': 2, 'data': 'things}])
-	{1: {'data': 'stuff'}, 2: {'data': 'things'}}
+    >>> refactor_json_data([{'id': 1, 'data': 'stuff'},
+                            {'id': 2, 'data': 'things}])
+    {1: {'data': 'stuff'}, 2: {'data': 'things'}}
 
-	This is useful for fast lookups.'''
+    This is useful for fast lookups.'''
 
-	out_dict = {}
-	for item in json_list:
-		uid = item.pop(master_key)
-		out_dict[uid] = item
+    out_dict = {}
+    for item in json_list:
+        uid = item.pop(master_key)
+        out_dict[uid] = item
 
-	return out_dict
+    return out_dict
