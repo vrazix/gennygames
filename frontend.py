f90c08de38302f7dbcb255b6ccfeacbe32e34dff
vrazix <vrazix@gmail.com>
2018-07-02 21:45:26 -0400

    import url_for

--- frontend.py
+++ frontend.py
@@ -1,5 +1,5 @@
 from boardgames import viable_game_list, load_data
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, url_for
 app = Flask(__name__)
 app.add_url_rule('/favicon.ico',
                  redirect_to=url_for('static', filename='favicon.ico'))
